e23578c5c2c654ddac666051ff23cd24210e36a1
techchandu <vcsreddy2004@gmail.com>
2024-10-11 09:55:05 +0530

    optimised code to avoid problem long term problem

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
+from bson import ObjectId
 from flask import Flask, render_template, request, jsonify, redirect
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-import random
-import string
-
 load_dotenv()
 
 app = Flask(__name__)
@@ -14,35 +12,9 @@ client = MongoClient(os.getenv('MONGO_PATH'), int(os.getenv('MONGO_PORT')))
 database = client.ShortUrlDatabase
 ShortUrlDatabase = database.URLData
 
-
-# Functions
-def generate_random_string(length=5):
-    '''
-    This function gives a combination of lowercase and uppercase letters of k length
-    '''
-    print('generating a random keyword')
-    letters = string.ascii_letters
-    random_string = ''.join(random.choices(letters, k=length))
-    print('keyword generated: '+str(random_string))
-    return random_string
-
-
 def is_keyword_present(keyword):
-    '''
-    Function to check if the current keyword is present in the DB or not
-    '''
-    print('Fetching if keyword is present in DB: '+str(keyword))
-    present = 0
-    for item in ShortUrlDatabase.find():
-        if (item['keyword'] == keyword):
-            print('keyword present in DB: '+str(keyword))
-            present = 1
-    print('keyword not present in DB: '+str(keyword))
-    return present
-
-# Routes
-
-
+    """Check if the keyword is already present in the database"""
+    return ShortUrlDatabase.count_documents({'keyword': keyword}) > 0
 @app.route('/')
 def home():
     '''
@@ -50,58 +22,44 @@ def home():
     '''
     print('Home Page API Called, rendering template')
     return render_template('index.html')
-
-
-@app.route('/getURL')
-def currentURl():
-    '''
-    This function will return the current working URL, something like 'http://127.0.0.1:5000/'
-    '''
-    print('getURL API Called')
-    current_url = request.host_url
-    print('Current URL: '+str(current_url))
-    return f'The app is running on {current_url}'
-
-
 @app.route('/shorten', methods=['POST'])
 def shortenAPI():
     '''
     This method will shorten the link and give the short URL
     '''
-    # Logic
-    # This end point should check if that URL is already present in the DB or not
-    # This end point should take only POST calls and return the shortened version of the URL
-    # do: Handle Get cases
     print('URL Shorten Called')
-    print('Mongo Connection: '+str(os.getenv('MONGO_PATH')[:6]))
-    
+    print('Mongo Connection: ' + str(os.getenv('MONGO_PATH')[:6]))
+
     if request.method == 'POST':
         longUrl = request.json.get("longUrl")
         keyword = request.json.get("keyword")
 
         # Logs
         print('Long URL Received: ' + str(longUrl))
-        print('Keyword Received: '+str(keyword))
+        print('Keyword Received: ' + str(keyword))
 
-        if keyword == '':
-            keyword = generate_random_string()
-            print('New keyword generated:' + str(keyword))
-            
-
-        if is_keyword_present(keyword) == 0:
-            print('Keyword is not present, inserting into DB')
-            ShortUrlDatabase.insert_one(
-                {'keyword': keyword, 'url': longUrl, 'clicks': 0})
-            print('DB insert successful')
+        # If no keyword is provided, use MongoDB's _id as the short URL
+        if not keyword:
+            # Insert the long URL into the DB and get the inserted document's _id
+            inserted_document = ShortUrlDatabase.insert_one({'url': longUrl, 'clicks': 0})
+            keyword = str(inserted_document.inserted_id)
+            print('No keyword provided, using MongoDB _id: ' + keyword)
         else:
-            print('Keyword is present, throwing error')
-            return jsonify({'shortUrl': 'The Keyword Already Exists, Choose a Different One'})
+            # If keyword exists, return an error
+            if is_keyword_present(keyword):
+                print('Keyword is present, throwing error')
+                return jsonify({'error': 'The Keyword Already Exists, Choose a Different One'}), 400
+            
+            # Insert with the provided keyword
+            ShortUrlDatabase.insert_one({'keyword': keyword, 'url': longUrl, 'clicks': 0})
+            print('DB insert successful with provided keyword')
+
+        # Return the short URL with either the keyword or MongoDB's _id
+        return jsonify({'shortUrl': str(request.host_url) + str(keyword)})
 
     if request.method == 'GET':
-        print('Called get method on shorten end-point, throwing error')
-        return "GET Method Not Allowed On This End Point"
-
-    return jsonify({'shortUrl': str(request.host_url)+str(keyword)})
+        print('Called GET method on shorten end-point, throwing error')
+        return "GET Method Not Allowed On This End Point", 405
 
 
 @app.route('/analytics', methods=['GET', 'POST'])
@@ -127,12 +85,12 @@ def reroute(keyword):
     link_status = 0
     print('Finding the URL Keyword')
     print('Mongo Connection: '+str(os.getenv('MONGO_PATH')[:6]))
-    for item in ShortUrlDatabase.find():
-        if (item['keyword'] == keyword):
-            redirection = item['url']
-            # Write Logs Here
-            print('Short URL <> Long URL mapping found in DB')
-            link_status = 1
+    item = ShortUrlDatabase.find_one({'$or': [{'keyword': keyword}, {'_id': ObjectId(keyword)}]})
+    if item:
+        redirection = item['url']
+        # Write Logs Here
+        print('Short URL <> Long URL mapping found in DB')
+        link_status = 1
     if (link_status == 0):
         print('Link Not Found in DB')
         return "Link Not Found"
